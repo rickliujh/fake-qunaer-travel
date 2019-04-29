614406269f57a07239f14033b0b1b904d3d9b90c
Rich <liujh88888@sina.cn>
2019-04-29 14:36:57 +0800

    fix timer optimize performance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -48,7 +48,7 @@ export default {
         if (this.timer) {
           clearTimeout(this.timer)
         }
-        setTimeout(() => {
+        this.timer = setTimeout(() => {
           let touchY = e.touches[0].clientY - 79
           let index = Math.floor((touchY - this.startY) / 20)
           if (index >= 0 && index < this.letters.length) {

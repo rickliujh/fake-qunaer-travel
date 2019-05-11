037eebc4fd9ddbbe9818f29c1bd27eb8c1327e8e
Rich <liujh88888@sina.cn>
2019-05-11 11:17:43 +0800

    detail list structure

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
+  <div class="container">
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <!-- <detail-introduction></detail-introduction> -->
+    <detail-list :list="list"></detail-list>
     <div class="content"></div>
   </div>
 </template>
@@ -9,16 +11,43 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+// import DetailIntroduction from './components/Introduction'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    // DetailIntroduction,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三张联票',
+          'children': [{
+            'title': '成人三馆联票 - 某一连锁店销售'
+          }]
+        }, {
+          title: '成人五张联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.content
-  height 50rem
+.container
+  background #eee
+  .content
+    height 50rem
 </style>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div
+      class="item"
+      v-for="(item, index) of list"
+      :key="index"
+    >
+      <div class="item-title border-bottom">
+        <span class="mp-ticketype-ticket"></span>
+        {{item.title}}
+      </div>
+      <div class="item-children" v-if="item.children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.container
+  background #fff
+  .item-title
+    line-height .68rem
+    padding-left .66rem
+    font-size .32rem
+  .item-children
+    padding 0 .3rem
+  .mp-ticketype-ticket
+    display inline-block
+    position absolute
+    width .36rem
+    height .36rem
+    top .15rem
+    left .2rem
+    background url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+    background-image url(http://s.qunarzz.com/piao/image/touch/sight/detail.png)
+    background-position-x 0px
+    background-position-y -0.45rem
+    background-size .4rem 3rem
+    background-repeat-x no-repeat
+    background-repeat-y no-repeat
+    background-attachment initial
+    background-origin initial
+    background-clip initial
+    background-color initial
+    margin-right .1rem
+    background-size .4rem 3rem
+</style>

748c5fddb689178e8a38924c8afb20e46acb9d79
Rich <liujh88888@sina.cn>
2019-05-13 19:37:02 +0800

    detail fix

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -28,11 +28,44 @@ export default {
         ticketType: [{
           title: '西溪湿地成人门票（龙舌嘴主入口）',
           price: '70',
-          tickets: {}
+          tickets: [{
+            title: '【龙舌嘴入口】西溪洪园成人票',
+            price: '70',
+            status: '可订明日',
+            explanation: [{text: '随时退'}],
+            company: '同程旅游'
+          }]
         }, {
           title: '西溪湿地成人门车船联票（龙舌嘴主入口）',
           price: '120',
-          tickets: {}
+          tickets: [{
+            title: '西溪湿地成人门车船联票（龙舌嘴主入口）',
+            price: '120',
+            status: '可订明日',
+            explanation: [
+              {text: '自营'},
+              {text: '无需换票'},
+              {text: '随时退'},
+              {text: '赠券￥10.0'}
+            ],
+            company: '去哪儿直销'
+          }, {
+            title: '【自动出票】洪园（龙舌嘴入口）门票+车船联票套餐成人票',
+            price: '120',
+            status: '可订明日',
+            explanation: [
+              {text: '随时退'}
+            ],
+            company: '杭州易游'
+          }, {
+            title: '【即买即用】西溪湿地洪园（龙舌嘴入口）大门票+船票+电瓶车票',
+            price: '120',
+            status: '可订明日',
+            explanation: [
+              {text: '随时退'}
+            ],
+            company: '游在路上'
+          }]
         }]
       }, {
         title: '学生票'
@@ -48,7 +81,7 @@ export default {
 
 <style lang="stylus" scoped>
 .container
-  background #eee
+  background #f5f5f5
   .content
     height 50rem
 </style>
